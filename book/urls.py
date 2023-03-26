30bc608cb8fd32105672adceb82cf30af635b326
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-03-26 17:08:27 +0300

    Add views for book endpoints

--- book/urls.py
+++ book/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from book.views import BookViewSet
 
 app_name = "book"
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("", BookViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
