a6f86c4a7608b6ab5ceb44d3694e1aae04394619
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-03-26 17:26:35 +0300

    Add separate serializer and view for books list

--- book/serializers.py
+++ book/serializers.py
@@ -8,3 +8,10 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
+
+
+class BookListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author")
--- book/views.py
+++ book/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets
 
 from book.models import Book
-from book.serializers import BookSerializer
+from book.serializers import BookSerializer, BookListSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
-    serializer_class = BookSerializer
     queryset = Book.objects.all()
+
+    def get_serializer_class(self) -> BookSerializer | BookListSerializer:
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
