3152060018e7564829f0bd3e1b6a13c30fb5acac
yuliia-stopkyna <styopkinayuliya@gmail.com>
2023-03-26 16:40:08 +0300

    Start book app, add book model and register in admin

---
+++ book/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
+    cover = models.CharField(max_length=10, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
